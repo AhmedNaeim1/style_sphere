fe11f339fa0c90579b2184c324960ce494c0dd1e
Ahmed Naeim <ahmed.naiim@ngu.edu.eg>
2024-04-20 00:48:36 +0200

    login

---
+++ lib/presentation/screens/Authentication/login.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sizer/sizer.dart';
+import 'package:style_sphere/businessLogic/cubits/user_Cubit.dart';
+import 'package:style_sphere/constants.dart';
+import 'package:style_sphere/presentation/widgets/constant_Widgets.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool errorEmail = false;
+
+  bool errorPassword = false;
+
+  bool errorConfirmPassword = false;
+
+  bool buttonColor = false;
+
+  String errorMessage = '';
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = BlocProvider.of<userCubit>(context);
+
+    return BlocProvider<userCubit>(
+      create: (context) => userCubit(),
+      child: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: const SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.dark,
+          statusBarBrightness: Brightness.light,
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: LayoutBuilder(
+            builder: (context, constraint) {
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraint.maxHeight),
+                  child: IntrinsicHeight(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/loginPage.png"),
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.center,
+                        ),
+                      ),
+                      child: SafeArea(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(18.0, 0, 18, 18),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              buildSizedBox(5.h),
+                              buildAppBar("Login", context),
+                              buildSizedBox(5.h),
+                              buildTextFieldSection(
+                                  "Email",
+                                  _emailController,
+                                  "Enter your email",
+                                  true,
+                                  errorEmail,
+                                  (value) {}),
+                              buildSizedBox(1.h),
+                              Column(
+                                children: [
+                                  buildTextFieldSection(
+                                      "Password",
+                                      _passwordController,
+                                      "Enter your password",
+                                      true,
+                                      errorPassword,
+                                      (value) {},
+                                      hidden: true),
+                                  errorMessage.isNotEmpty
+                                      ? Row(
+                                          children: [
+                                            Icon(
+                                              Icons.info_outline,
+                                              color: redColor,
+                                              size: 14.sp,
+                                            ),
+                                            SizedBox(
+                                              width: 1.w,
+                                            ),
+                                            buildErrorText(text: errorMessage),
+                                          ],
+                                        )
+                                      : buildSizedBox(0.h),
+                                  Align(
+                                    alignment: Alignment.centerRight,
+                                    child: buildCustomText(
+                                      text: "Forgot Password?",
+                                      color: primaryColor,
+                                      fontSize: 10,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              buildSizedBox(4.h),
+                              CustomElevatedButton(
+                                text: "Login",
+                                onPressed: () {
+                                  if (_emailController.text.isEmpty &&
+                                      _passwordController.text.isEmpty) {
+                                    setState(() {
+                                      errorEmail = true;
+                                      errorPassword = true;
+                                      errorMessage =
+                                          "Email and Password are required";
+                                    });
+                                  } else if (_emailController.text.isEmpty) {
+                                    setState(() {
+                                      errorEmail = true;
+                                      errorMessage = "Email is required";
+                                    });
+                                  } else if (_passwordController.text.isEmpty) {
+                                    setState(() {
+                                      errorPassword = true;
+                                      errorMessage = "Password is required";
+                                    });
+                                  } else {
+                                    setState(() {
+                                      errorEmail = false;
+                                      errorPassword = false;
+                                      errorMessage = "";
+                                    });
+                                    cubit.loginWithEmailPassword(
+                                        _emailController.text,
+                                        _passwordController.text,
+                                        context);
+                                  }
+                                },
+                                color: true,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screens/login_Register_Page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sizer/sizer.dart';
+import 'package:style_sphere/presentation/router.dart';
+import 'package:style_sphere/presentation/widgets/constant_Widgets.dart';
+
+class LoginRegisterPage extends StatelessWidget {
+  const LoginRegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: LayoutBuilder(
+          builder: (context, constraint) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraint.maxHeight),
+                child: IntrinsicHeight(
+                  child: SafeArea(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(18.0, 0, 18, 18),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          buildSizedBox(10.h),
+                          Expanded(
+                            child: Container(
+                              height: double.infinity,
+                              width: double.infinity,
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                      "assets/loginRegisterPage.png"),
+                                  fit: BoxFit.fitWidth,
+                                  alignment: Alignment.center,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const Spacer(),
+                          SizedBox(
+                            width: double.infinity,
+                            height: 6.h,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                    context, AppRoutes.firstStep);
+                              },
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.white),
+                                side: MaterialStateProperty.all<BorderSide>(
+                                    const BorderSide(color: Colors.black)),
+                                elevation: MaterialStateProperty.all(0),
+                              ),
+                              child: const Text(
+                                "Register",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                          buildSizedBox(2.h),
+                          CustomElevatedButton(
+                            text: 'Login',
+                            onPressed: () {
+                              Navigator.pushNamed(context, AppRoutes.login);
+                            },
+                            color: true,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
