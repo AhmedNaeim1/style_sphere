96aadf0a51fb6185d1a1e27b45934a8f7ee9f16e
Ahmed Naeim <ahmed.naiim@ngu.edu.eg>
2024-06-20 21:11:37 +0300

    change image

--- lib/presentation/screens/Settings/editProfile/edit_profile.dart
+++ lib/presentation/screens/Settings/editProfile/edit_profile.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,6 +12,7 @@ import 'package:style_sphere/data/repositories/user_repository.dart';
 import 'package:style_sphere/presentation/constant_widgets/appBars.dart';
 import 'package:style_sphere/presentation/constant_widgets/constant_Widgets.dart';
 import 'package:style_sphere/presentation/constant_widgets/textFields.dart';
+import 'package:style_sphere/presentation/functions/constant_functions.dart';
 import 'package:style_sphere/presentation/router.dart';
 
 class EditProfilePage extends StatefulWidget {
@@ -25,6 +27,8 @@ class EditProfilePage extends StatefulWidget {
 class _EditProfilePageState extends State<EditProfilePage> {
   bool isEditingFirst = false;
   bool isEditingSecond = false;
+  bool _isLoading = false;
+  String imagePath = "";
 
   final TextEditingController firstNameController = TextEditingController();
   final TextEditingController lastNameController = TextEditingController();
@@ -43,7 +47,8 @@ class _EditProfilePageState extends State<EditProfilePage> {
         ),
         child: Scaffold(
           backgroundColor: Colors.white,
-          appBar: buildAppBar("Edit Profile", context, 14.sp),
+          appBar:
+              buildLeadingAppBar("Edit Profile", context, 14.sp, widget.user),
           body: Container(
             decoration: const BoxDecoration(
               image: DecorationImage(
@@ -58,160 +63,186 @@ class _EditProfilePageState extends State<EditProfilePage> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Stack(children: [
-                    Container(
-                      margin: EdgeInsets.only(top: 21.5.h),
-                      decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(0.8),
-                        borderRadius: BorderRadius.circular(20.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.2),
-                            spreadRadius: 3,
-                            blurRadius: 5,
-                          ),
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(18.0, 0, 18, 18),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 8.h,
+                  Stack(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 21.5.h),
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.8),
+                          borderRadius: BorderRadius.circular(20.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.2),
+                              spreadRadius: 3,
+                              blurRadius: 5,
                             ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: buildNameFieldEditProfile(
-                                    "First Name",
-                                    widget.user.name.toString().split(" ")[0],
-                                    firstNameController,
-                                    isEditingFirst,
-                                    (newValue) {
-                                      setState(() {
-                                        isEditingFirst = newValue;
-                                        isEditingSecond = false;
-                                        widget.user.name =
-                                            "${firstNameController.text} ${lastNameController.text}";
-                                      });
-                                      cubit.updateUser(
-                                        widget.user.userID!,
-                                        widget.user,
-                                      );
-                                    },
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 5.w,
-                                ),
-                                Expanded(
-                                  child: buildNameFieldEditProfile(
-                                    "Last Name",
-                                    widget.user.name.toString().split(" ")[1],
-                                    lastNameController,
-                                    isEditingSecond,
-                                    (newValue) {
-                                      setState(() {
-                                        isEditingSecond = newValue;
-                                        isEditingFirst = false;
-                                        widget.user.name =
-                                            "${firstNameController.text} ${lastNameController.text}";
-                                      });
-                                      cubit.updateUser(
-                                        widget.user.userID!,
-                                        widget.user,
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ],
-                            ),
-                            buildSizedBox(2.h),
-                            buildEditProfileRow(
-                                "Change Email", AppRoutes.newEmail, context, {
-                              "user": json.encode(widget.user),
-                            }),
-                            buildSizedBox(1.h),
-                            const Divider(
-                              color: Colors.grey,
-                              thickness: 0.2,
-                            ),
-                            buildSizedBox(1.h),
-                            buildEditProfileRow("Change Password",
-                                AppRoutes.changePassword, context, {
-                              "user": json.encode(widget.user),
-                            }),
-                            buildSizedBox(1.h),
-                            const Divider(
-                              color: Colors.grey,
-                              thickness: 0.2,
-                            ),
-                            buildSizedBox(1.h),
-                            buildEditProfileRow(
-                              "Update Preferences",
-                              AppRoutes.preferences,
-                              context,
-                              {
-                                'preferences': {
-                                  "Style": widget.user.preferredStyles!
-                                      .map((value) =>
-                                          value.toLowerCase() == 'true')
-                                      .toList(),
-                                  "Material": widget.user.preferredMaterials!
-                                      .map((value) =>
-                                          value.toLowerCase() == 'true')
-                                      .toList(),
-                                  "Occasion": widget.user.preferredOccasions!
-                                      .map((value) =>
-                                          value.toLowerCase() == 'true')
-                                      .toList(),
-                                },
-                                'profile': true,
-                                'preferencesPage': 'Style',
-                              },
-                            ),
-                            buildSizedBox(1.h),
                           ],
                         ),
-                      ),
-                    ),
-                    Align(
-                        alignment: Alignment.center,
-                        child: SizedBox(
-                          child: CircleAvatar(
-                            radius: 14.h,
-                            backgroundImage: NetworkImage(
-                              widget.user.profilePictureUrl.toString() == "null"
-                                  ? "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"
-                                  : widget.user.profilePictureUrl.toString(),
-                            ),
-                            child: CircleAvatar(
-                              radius: 12.h,
-                              backgroundColor: Colors.transparent,
-                              child: GestureDetector(
-                                onTap: () {
-                                  // cubit.uploadProfilePicture(
-                                  //   widget.user.userID!,
-                                  //   widget.user,
-                                  // );
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(18.0, 0, 18, 18),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 8.h,
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: buildNameFieldEditProfile(
+                                      "First Name",
+                                      widget.user.name.toString().split(" ")[0],
+                                      firstNameController,
+                                      isEditingFirst,
+                                      (newValue) {
+                                        setState(() {
+                                          isEditingFirst = newValue;
+                                          isEditingSecond = false;
+                                          widget.user.name =
+                                              "${firstNameController.text} ${lastNameController.text}";
+                                        });
+                                        cubit.updateUser(
+                                          widget.user.userID!,
+                                          widget.user,
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 5.w,
+                                  ),
+                                  Expanded(
+                                    child: buildNameFieldEditProfile(
+                                      "Last Name",
+                                      widget.user.name.toString().split(" ")[1],
+                                      lastNameController,
+                                      isEditingSecond,
+                                      (newValue) {
+                                        setState(() {
+                                          isEditingSecond = newValue;
+                                          isEditingFirst = false;
+                                          widget.user.name =
+                                              "${firstNameController.text} ${lastNameController.text}";
+                                        });
+                                        cubit.updateUser(
+                                          widget.user.userID!,
+                                          widget.user,
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              buildSizedBox(2.h),
+                              buildEditProfileRow(
+                                  "Change Email", AppRoutes.newEmail, context, {
+                                "user": json.encode(widget.user),
+                              }),
+                              buildSizedBox(1.h),
+                              const Divider(
+                                color: Colors.grey,
+                                thickness: 0.2,
+                              ),
+                              buildSizedBox(1.h),
+                              buildEditProfileRow("Change Password",
+                                  AppRoutes.changePassword, context, {
+                                "user": json.encode(widget.user),
+                              }),
+                              buildSizedBox(1.h),
+                              const Divider(
+                                color: Colors.grey,
+                                thickness: 0.2,
+                              ),
+                              buildSizedBox(1.h),
+                              buildEditProfileRow(
+                                "Update Preferences",
+                                AppRoutes.preferences,
+                                context,
+                                {
+                                  'preferences': {
+                                    "Style": widget.user.preferredStyles!
+                                        .map((value) =>
+                                            value.toLowerCase() == 'true')
+                                        .toList(),
+                                    "Material": widget.user.preferredMaterials!
+                                        .map((value) =>
+                                            value.toLowerCase() == 'true')
+                                        .toList(),
+                                    "Occasion": widget.user.preferredOccasions!
+                                        .map((value) =>
+                                            value.toLowerCase() == 'true')
+                                        .toList(),
+                                  },
+                                  'profile': true,
+                                  'preferencesPage': 'Style',
                                 },
-                                child: Align(
-                                  alignment: Alignment.bottomRight,
-                                  child: CircleAvatar(
-                                    backgroundColor: primaryColor,
-                                    radius: 22.0,
-                                    child: Icon(
-                                      Icons.edit,
-                                      size: 3.h,
-                                      color: Colors.white,
+                              ),
+                              buildSizedBox(1.h),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            CircleAvatar(
+                              radius: 14.h,
+                              backgroundImage: NetworkImage(
+                                widget.user.profilePictureUrl.toString() ==
+                                        "null"
+                                    ? "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"
+                                    : widget.user.profilePictureUrl.toString(),
+                              ),
+                              child: CircleAvatar(
+                                radius: 12.h,
+                                backgroundColor: Colors.transparent,
+                                child: GestureDetector(
+                                  onTap: () async {
+                                    setState(() {
+                                      _isLoading = true;
+                                    });
+
+                                    imagePath = await handleImage();
+                                    File image = File(imagePath);
+
+                                    var textResponse = await uploadImageFile(
+                                      image,
+                                      widget.user.userID.toString() +
+                                          "_" +
+                                          widget.user.name.toString(),
+                                    );
+
+                                    setState(() {
+                                      widget.user.profilePictureUrl =
+                                          textResponse;
+                                      _isLoading = false;
+                                    });
+
+                                    cubit.updateUser(
+                                        widget.user.userID!, widget.user);
+                                  },
+                                  child: Align(
+                                    alignment: Alignment.bottomRight,
+                                    child: CircleAvatar(
+                                      backgroundColor: primaryColor,
+                                      radius: 22.0,
+                                      child: Icon(
+                                        Icons.edit,
+                                        size: 3.h,
+                                        color: Colors.white,
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                        )),
-                  ]),
+                            if (_isLoading) CircularProgressIndicator(),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
