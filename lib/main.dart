8616be1570b6291674acb8d642d3e0c95d5a6dcd
Ahmed Naeim <ahmed.naiim@ngu.edu.eg>
2024-06-20 21:16:30 +0300

    main calling splash screen always

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,14 +15,18 @@ import 'package:style_sphere/data/repositories/product_repository.dart';
 import 'package:style_sphere/data/repositories/user_repository.dart';
 import 'package:style_sphere/presentation/router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Sizer(
@@ -49,42 +54,17 @@ class MyApp extends StatelessWidget {
             ),
           ],
           child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              appBarTheme: const AppBarTheme(
-                systemOverlayStyle: SystemUiOverlayStyle.dark,
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                appBarTheme: const AppBarTheme(
+                  systemOverlayStyle: SystemUiOverlayStyle.dark,
+                ),
               ),
-            ),
-            routes: AppRoutes.define(),
-            onGenerateRoute: AppRoutes.generateRoute,
-            initialRoute: AppRoutes.navbar,
-          ),
+              routes: AppRoutes.define(),
+              onGenerateRoute: AppRoutes.generateRoute,
+              initialRoute: AppRoutes.splashScreen),
         );
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Home Page',
-            style: TextStyle(fontSize: 20),
-          ),
-        ],
-      ),
     );
   }
 }
