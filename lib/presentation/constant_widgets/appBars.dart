fad8d810216f2001d25a216fe7d4eaa388d34f9e
Ahmed Naeim <ahmed.naiim@ngu.edu.eg>
2024-06-20 21:11:15 +0300

    added appbars

--- lib/presentation/constant_widgets/appBars.dart
+++ lib/presentation/constant_widgets/appBars.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:style_sphere/constants.dart';
+import 'package:style_sphere/data/models/user_data.dart';
+import 'package:style_sphere/presentation/router.dart';
 
 PreferredSizeWidget buildAppBar(
     String title, BuildContext context, double fontSize) {
@@ -14,6 +18,33 @@ PreferredSizeWidget buildAppBar(
       ),
       onPressed: () {
         Navigator.pop(context);
+      },
+    ),
+    title: Text(
+      title,
+      style: TextStyle(
+        fontFamily: 'Gabarito',
+        color: darkBlueColor,
+        fontSize: fontSize,
+        fontWeight: FontWeight.normal,
+      ),
+    ),
+  );
+}
+
+PreferredSizeWidget buildLeadingAppBar(
+    String title, BuildContext context, double fontSize, UserData user) {
+  return AppBar(
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+    leading: IconButton(
+      icon: Icon(
+        Icons.arrow_back_ios,
+        color: darkBlueColor,
+      ),
+      onPressed: () {
+        Navigator.pushNamed(context, AppRoutes.settings,
+            arguments: jsonEncode(user));
       },
     ),
     title: Text(
